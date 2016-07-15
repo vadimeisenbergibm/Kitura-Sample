5b131405427be546e2230d54d4ecfd970a66b95a
Shmuel Kallner <kallner@il.ibm.com>
2016-07-15 13:24:53 +0300

    Migration 0620 (#20)
    * Updated version and CI settings
    
    * For IBM-Swift/Kitura#590
    
    * IBM-Swift/Kitura#590 Updated dependency version

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 21),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 10),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 12),
         //.Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 0, minor: 13),
     ],
     exclude: ["Makefile", "Kitura-Build"])
