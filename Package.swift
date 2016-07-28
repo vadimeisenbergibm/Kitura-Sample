de9e65601c294088630cbfff2b4d5a18fc1572fa
Daniel Firsht <dfirsht@us.ibm.com>
2016-07-28 14:32:41 -0500

    Updated to new version.

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 23),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 24),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 13),
         //.Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 0, minor: 13),
     ],
