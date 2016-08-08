9433c449ed46e7f7bb6c665c264a7c06a1a22d3e
Shmuel Kallner <kallner@il.ibm.com>
2016-08-08 14:05:21 +0300

    IBM-Swift/Kitura#644 Upgraded swift version and dependencies to latest Kitura and HeliumLogger

--- Package.swift
+++ Package.swift
@@ -25,8 +25,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 24),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 13),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 25),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 14),
         //.Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 0, minor: 13),
     ],
     exclude: ["Makefile", "Kitura-Build"])
