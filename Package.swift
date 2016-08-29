078aba51e5e8618d3c85cd5327b63c57fcbe3df2
Vadim Eisenberg <vadime@il.ibm.com>
2016-08-29 12:03:08 +0300

    updated dependency versions in Package.swift
    set version of https://github.com/IBM-Swift/Kitura.git to 0.28.0
    set version of https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git to 0.28.0

--- Package.swift
+++ Package.swift
@@ -25,8 +25,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 27),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 28),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 15),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 0, minor: 27),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 0, minor: 28),
     ],
     exclude: ["Makefile", "Package-Builder"])
