12bc3be49ad97274651c2fe36f66fed4925e94cb
Vadim Eisenberg <vadime@il.ibm.com>
2016-08-29 12:01:06 +0300

    Added using GRMustache on macOS only (#30)
    * IBM-Swift/Kitura#697 added updated dependency on GRMustache
    
    * IBM-Swift/Kitura#697 added updated code that uses GRMustache

--- Package.swift
+++ Package.swift
@@ -27,6 +27,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 27),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 15),
-        //.Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 0, minor: 13),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 0, minor: 27),
     ],
     exclude: ["Makefile", "Package-Builder"])
