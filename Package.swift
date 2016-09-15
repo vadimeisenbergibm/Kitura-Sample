7e738d5e3c13be3cf50783d4f5646d3e5207ba5d
Ian Partridge <i.partridge@uk.ibm.com>
2016-09-15 14:51:04 +0100

    updated dependency versions in Package.swift (#36)
    set version of https://github.com/IBM-Swift/Kitura.git to 0.32.0
    set version of https://github.com/IBM-Swift/HeliumLogger.git to 0.17.0
    set version of https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git to 0.32.0

--- Package.swift
+++ Package.swift
@@ -25,8 +25,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 31),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 16),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 0, minor: 30),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 32),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 17),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 0, minor: 32),
     ],
     exclude: ["Makefile", "Package-Builder"])
