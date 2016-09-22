7cd9013a4279c0124b784d013b1c5c5280dbe6e9
Shmuel Kallner <kallner@il.ibm.com>
2016-09-22 17:18:00 +0300

    updated dependency versions in Package.swift (#38)
    set version of https://github.com/IBM-Swift/Kitura.git to 1.0.0
    set version of https://github.com/IBM-Swift/HeliumLogger.git to 1.0.0
    set version of https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git to 1.0.0
    set version of https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git to 1.0.0

--- Package.swift
+++ Package.swift
@@ -25,9 +25,9 @@ let package = Package(
         )
     ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 32),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 17),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 0, minor: 32),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", majorVersion: 0, minor: 33),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", majorVersion: 1, minor: 0),
     ],
     exclude: ["Makefile", "Package-Builder"])
