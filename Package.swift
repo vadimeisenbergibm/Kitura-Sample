6f1395be3b36ecab0c86d86eb46f3b1fccc35872
Shmuel Kallner <kallner@il.ibm.com>
2016-11-03 17:30:31 +0200

    Upgrade to Swift 3.0.1 and Kitura 1.1 (#46)
    * IBM-Swift/Kitura#832 Upgraded to Swift 3.0.1
    
    * IBM-Swift/Kitura#832 Updated to lated versions of dependencies

--- Package.swift
+++ Package.swift
@@ -19,9 +19,9 @@ import PackageDescription
 let package = Package(
     name: "KituraSample",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", majorVersion: 1, minor: 1),
     ],
     exclude: ["Makefile", "Package-Builder"])
