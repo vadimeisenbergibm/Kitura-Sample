53ed96404746aedc17dffdd7ea6515b4d6219e8e
Ian Partridge <i.partridge@uk.ibm.com>
2016-11-17 12:01:27 +0000

    Update to Kitura 1.2

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
             Target(name: "KituraSampleRouter")],
 
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 2),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 1),
         .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 1, minor: 1),
         .Package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", majorVersion: 1, minor: 1),
