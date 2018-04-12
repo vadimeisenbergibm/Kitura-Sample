45998209a57a462499949c4230f8e2e36ae2f4f5
David Jones <djones6dev@gmail.com>
2018-04-12 12:00:54 +0100

    Swift 4.1 support (#83)

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.2.0")),
         .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", .upToNextMinor(from: "1.7.0")),
-        .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", .upToNextMinor(from: "1.8.0")),
+        .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", .upToNextMinor(from: "1.9.0")),
         .package(url: "https://github.com/IBM-Swift/Kitura-Markdown", .upToNextMinor(from: "0.9.0")),
         .package(url: "https://github.com/IBM-Swift/Kitura-WebSocket.git", .upToNextMinor(from: "1.0.1"))
     ],
